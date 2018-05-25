56e0b2fa5831a4c22595800f48dc423c3e86fee9
Raj <32778886+rk16449@users.noreply.github.com>
2018-05-25 23:55:29 +0100

    Internal Structure

--- AdsoofPractise/src/LinkedList.java
+++ AdsoofPractise/src/LinkedList.java
@@ -1,9 +1,17 @@
 
 public class LinkedList {
+	
+	class Cell<T>{
+		T first;
+		Cell<T> next;
+		Cell(T _first, Cell<T> _next){
+			this.first = _first;
+			this.next = _next;
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
