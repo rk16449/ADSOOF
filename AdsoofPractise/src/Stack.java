917f305374c6ab290f7dfe44615b1306b71b9336
Raj <32778886+rk16449@users.noreply.github.com>
2018-05-28 23:58:05 +0100

    added all methods needed

--- AdsoofPractise/src/Stack.java
+++ AdsoofPractise/src/Stack.java
@@ -5,5 +5,53 @@ public class Stack {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	
+	private int maxSize;
+	
+	private int topVariable;
+	
+	private Data[] list;
+	
+	
+	public void isEmptyStack() {
+		
+	}
+	
+	public void isFullStack() {
+		
+	}
+	
+	public void initializeStack() {
+		
+	}
+	
+	public Stack() {
+		
+	}
+	
+	// Copy constructor
+	public Stack(Stack old) {
+		
+	}
+	
+	public void top() {
+		
+	}
+	
+	public void push() {
+		
+	}
+	
+	public void pop() {
+		
+	}
+	
+	public void copy() {
+		
+	}
+	
+	public void copyStack() {
+		
+	}
 }
