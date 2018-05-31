9ab7b6026294ff416d84dfb48185ffe8a7b4fe1d
Raj <32778886+rk16449@users.noreply.github.com>
2018-05-31 18:33:21 +0100

    completed Stack data structure

--- AdsoofPractise/src/Data.java
+++ AdsoofPractise/src/Data.java
@@ -1,4 +1,12 @@
 
 public class Data {
 
+	
+	public Data copyMe() {
+		return new Data(this);
+	}
+	
+	public Data(Data old) {
+		// Copy over any variables etc here
+	}
 }
--- AdsoofPractise/src/Stack.java
+++ AdsoofPractise/src/Stack.java
@@ -6,75 +6,112 @@ public class Stack {
 	}
 	
 	private int maxSize;
-	private int topElement;
-	private Data[] list;
+	private int topStack;
+	private Data[] stack;
 	
 	// Returns false if topElement is 0
 	public boolean isEmptyStack() {
-		return topElement != 0;
+		return topStack != 0;
 	}
 	
-	public boolean isFullStack() {
+	public boolean isStackFull() {
 		// Determine whether the stack is full 
 		
 		// Postcondition: returns true if the stack is empty, false otherwise
 		
-		return false;
+		return topStack == maxSize;
 	}
 	
 	public void initializeStack() {
 		for(int i=0; i<maxSize; i++) {
-			list[i] = null;
+			stack[i] = null;
 		}
-		topElement = 0;
+		topStack = 0;
 	}
 	
 	// Default constructor
 	public Stack() {
 		// Create array size 16
 		// Postcondition: The variable list contains the base of the array, topElement = 0 and maxSize = 16
-		this.maxSize = 16;
-		this.initializeStack();
+		maxSize = 16;
+		topStack = 0;
+		stack = new Data[maxSize];
 	}
 	
-	public Stack(int _stackSize) {
-		this.maxSize = _stackSize;
-		this.initializeStack();
+	public Stack(int size) {
+		if(size <= 0) {
+			maxSize = 16;
+		}else {
+			maxSize = size;
+		}
+		
+		topStack = 0;
+		stack = new Data[maxSize];
 	}
 	
 	// Copy constructor
-	public Stack(Stack old) {
-		
+	public Stack(Stack newStack) {
+		copy(newStack);
 	}
 	
 	public Data top() {
 		// Precondition: Stack exists and is not empty
-		
+		if(isEmptyStack()) {
+			// throw exception here
+		}
+			
 		// Postcondition: If stack empty throw exception, otherwise reference to a copy of te top element
-		return list[topElement];
+		return stack[topStack-1].copyMe();
 	}
 	
 	// Place value onto stack
 	public void push(Data element) {
 		// Precondition: Stack exists and is not full
 		
+		
 		// Postcondition: The stack is changed and new item is added to the top
+		
+		if(isStackFull()) {
+			// throw exception here
+		}
+		
+		stack[topStack] = element.copyMe();
+		
+		topStack++;
 	}
 	
 	// Remove element from stack
 	public void pop() {
 		// Precondition: The stack exists and is not empty
+		if(isEmptyStack()) {
+			//throw exception here
+		}
+		
+		
 		
 		// Postcondition: The stack is changed and the top element is removed from the stack.
 		// If the stack is empty throw exception
+		topStack--;
+		stack[topStack] = null;
 	}
 	
 	private void copy(Stack old) {
+		stack = null;
+		System.gc();
 		
+		maxSize = old.maxSize;
+		topStack = old.topStack;
+		
+		stack = new Data[maxSize];
+		
+		// copy old stack into this stack
+		for(int i=0; i<topStack; i++) {
+			stack[i] = old.stack[i].copyMe();
+		}
 	}
 	
-	public void copyStack(Stack old) {
-		if(this != old)
-			copy(old);
+	public void copyStack(Stack otherStack) {
+		if(this != otherStack)
+			copy(otherStack);
 	}
 }
