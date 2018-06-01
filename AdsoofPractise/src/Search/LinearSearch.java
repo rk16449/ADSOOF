3d650fec8bae9b20ae7e72c378497cf6654f44c2
Raj <32778886+rk16449@users.noreply.github.com>
2018-06-01 22:28:10 +0100

    added generic linear search

--- AdsoofPractise/src/Search/LinearSearch.java
+++ AdsoofPractise/src/Search/LinearSearch.java
@@ -1,6 +1,16 @@
 package Search;
 
 
-public class LinearSearch {
-
+public class LinearSearch <E> {
+	public static <E> boolean search(E obj, E[] arr) {
+		// Loop through generic arr
+		for(int i=0; i<arr.length; i++) {
+			// If we find a match return true
+			if(arr[i].equals(obj)) 
+				return true;
+		}
+		
+		// No match found
+		return false;
+	}
 }
