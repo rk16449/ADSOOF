144b236b42888146fa4b324f810c526369c36d99
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-14 23:11:30 +0100

    added JUnit; SingleHashTableTest.java

---
+++ test/SingleHashTableTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import dataStructures.SingleHashTable;
+
+class SingleHashTableTest {
+
+	@Test
+	void testConstructor() {
+		
+		// Setup Hash Table
+		SingleHashTable singleHT = new SingleHashTable(100);
+		
+	}
+
+}
