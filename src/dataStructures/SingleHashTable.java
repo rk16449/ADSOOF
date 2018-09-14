a3f122c846fee38657fc6a7b734da62c92f15d49
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-14 23:00:41 +0100

    added data structure; Single Hash Table (no chaining/linked lists)

---
+++ src/dataStructures/SingleHashTable.java
@@ -0,0 +1,8 @@
+package dataStructures;
+
+public class SingleHashTable {
+
+	// Generic HashTable without separate chaining (no linked lists)
+	// Uses prime numbers
+	// Uses an underlining array data structure
+}
