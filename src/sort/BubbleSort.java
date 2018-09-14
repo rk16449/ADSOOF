7855c6e36d6b2321c870185c38434d4d292d2db5
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-14 22:47:14 +0100

    removed capital Sort, renamed to lowercase package

---
+++ src/sort/BubbleSort.java
@@ -0,0 +1,11 @@
+package Sort;
+
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/sort/MergeSort.java
@@ -0,0 +1,6 @@
+package Sort;
+
+
+public class MergeSort {
+
+}
