c54b4e68e9787291a0caadd04d76e6d6e8f2d380
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-15 14:28:03 +0100

    updated; SingleHashTableTest.java; refactor/cleaning

--- test/SingleHashTableTest.java
+++ test/SingleHashTableTest.java
@@ -1,5 +1,5 @@
-import static org.junit.Assert.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +11,7 @@ import dataStructures.SingleHashTable;
 class SingleHashTableTest {
 	
 	SingleHashTable<String> singleHT;
+	String word;
 	
 	@Rule
 	public ExpectedException exception = ExpectedException.none();
@@ -18,43 +19,44 @@ class SingleHashTableTest {
 	
 	@BeforeEach
 	public void beforeEach() {
-		
 		// Setup Hash Table of 100 string objects
 		singleHT = new SingleHashTable<String>(String.class, 100);
+		
+		// Setup the test word
+		word = "Hello";
 	}
 
 	@Test
 	void testFindWhenInsertedString() {
-		
-		
-		String test = "Hello World";
-		
 		// Insert a test String object
-		singleHT.insert(test);
+		singleHT.insert(word);
 		
 		// we expect it to be true, but its actually whatever the find method returns
-		assertEquals(true, singleHT.find(test));
+		assertEquals(true, singleHT.find(word));
 	}
 	
 	@Test
 	void testFindWhenDeletedString() {
-		
-		String word = "Hello";
+		// Insert the word into the HT
 		singleHT.insert(word);
+		
+		// Test that it exists
+		assertEquals(true, singleHT.find(word));
 		
 		// Delete it from the HT
 		singleHT.delete(word);
 		
+		// Test that it no longer exists
 		assertEquals(false, singleHT.find(word));
 	}
 	
 	@Test
 	void throwsIndexOutOfBoundsOnHashTableFull() {
-		
 		// Tests the index out of bounds if we try to insert more than the hash table has
+		@SuppressWarnings("unused")
 		Throwable exception = assertThrows(IndexOutOfBoundsException.class, () -> {
 			for(int i=0; i<singleHT.getArraySize() + 100; i++) {
-				singleHT.insert("Hello" + i);
+				singleHT.insert(word + i);
 			}
 	    });
 	}
