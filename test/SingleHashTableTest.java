f7d4b0fecee23163e29840336c5ad3ec62f749b7
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-15 01:22:07 +0100

    updated Junit; tests for inserting and finding a string from hash table

--- test/SingleHashTableTest.java
+++ test/SingleHashTableTest.java
@@ -1,17 +1,29 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import dataStructures.SingleHashTable;
 
 class SingleHashTableTest {
-
-	@Test
-	void testConstructor() {
+	
+	SingleHashTable<String> singleHT;
+	
+	@BeforeEach
+	public void beforeEach() {
 		
 		// Setup Hash Table of 100 string objects
-		SingleHashTable<String> singleHT = new SingleHashTable<String>(String.class, 100);
+		singleHT = new SingleHashTable<String>(String.class, 100);
+	}
+
+	@Test
+	void testFindWhenInsertedString() {
+		String test = "Hello World";
+		singleHT.insert(test);
 		
+		// expect it to be true, but its actually ?
+		assertEquals(true, singleHT.find(test));
 	}
 
 }
