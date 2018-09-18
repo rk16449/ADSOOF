d6b8b62193ced1af84fe353c0eaf60c1cc849faa
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-19 00:27:57 +0100

    updated; LinkedHashTable.java; implements Interface and duplicate methods moved to super class

--- src/dataStructures/LinkedHashTable.java
+++ src/dataStructures/LinkedHashTable.java
@@ -2,69 +2,79 @@ package dataStructures;
 
 import java.lang.reflect.Array;
 
-public class LinkedHashTable <T> extends SuperHash{
+public class LinkedHashTable<T> extends SuperHash<T> implements IHashTable<T> {
 
 	// Generic Hash Table that supports separate chaining
 	// Uses a prime number as array size
 	// Underlining data structure is an array
-	
+
 	LinkedList<T>[] myArray;
-	
-	
+
+	@SuppressWarnings("unchecked")
 	public LinkedHashTable(Class<T> type, int size) {
 		// Make sure its prime sized
 		arraySize = getNextPrime(size);
-		
+
 		// Initialize the LinkedList array with the size given
 		myArray = (LinkedList<T>[]) Array.newInstance(type, size + 1);
-		
+
 		count = 0;
 	}
-	
-	
+
 	public void insert(T obj) {
 		// calculate hash indexes
 		// calculate the step required
-		
-		
+
+		if (count >= arraySize)
+			throw new IndexOutOfBoundsException("Hash Table is full!");
+
+		int hashIndex = hashIndexOne(obj);
+		int step = hashIndexTwo(obj);
+
 		// check that this node is null
-		
+
 		// if it isn't null then we have a linked list, just add it to the linked list
-		
+
 		// if its a duplicate, increase the linked list count instead
 	}
-	
-	
+
 	public void delete(T obj) {
-		
+
 		// calculate hash/step
-		
-		
+
 		// check if this index is not null
-		
-		// if its not null check inside the linkedlist if the value exists
-		
-		
-		// if it exists check the count value, if its 1 then we need to delete it entirely
-		
+
+		// if its not null check inside the LinkedList if the value exists
+
+		// if it exists check the count value, if its 1 then we need to delete it
+		// entirely
+
 		// if its above 1 just decrement the count instead
 	}
-	
-	
-	
+
 	public void update(T obj1, T obj2) {
 		// calculate the hash index
-		
+
 		// find obj1
-		
-		// if obj1 is found then remove it entirely, and re-insert obj2 into the linked list
+
+		// if obj1 is found then remove it entirely, and re-insert obj2 into the linked
+		// list
 	}
-	
+
 	public void updateCount(T obj, int count) {
 		// calculate hash/index
-		
+
 		// set the value of the count of the obj if found
 	}
-	
-	
+
+	@Override
+	public boolean exists(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void resize(int num) {
+		// TODO Auto-generated method stub
+	}
 }
