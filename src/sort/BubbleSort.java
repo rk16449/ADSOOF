ef0da23aad05152bbc8b94cdb1fd014e37b44c48
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-19 00:09:50 +0100

    updated; BubbleSort.java; check on compareTo now correct; updated classPath folder structure

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -2,11 +2,10 @@ package sort;
 
 public class BubbleSort<T> {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+	/**
+	 * Destructively changes the generic array into sorted form
+	 * @param arr
+	 */
 	public static <T extends Comparable<T>> void sort(T[] arr) {
 
 		// helper; to swap array values
@@ -18,7 +17,7 @@ public class BubbleSort<T> {
 			// Loop again, check with every other index
 			for (int z = 1; z < arr.length - i; z++) {
 				// compare the order of the objects here, store temp variable and swap
-				if (arr[z - 1].compareTo(arr[z]) > 1) {
+				if (arr[z - 1].compareTo(arr[z]) > 0) {
 					// swap
 					temp = arr[z - 1];
 					arr[z - 1] = arr[z];
