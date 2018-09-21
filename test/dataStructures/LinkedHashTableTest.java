a3232c9b894611e51c90f78b2324cffeeb76f200
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-22 00:34:40 +0100

    updated; LinkedHashTableTest.java; added JUnit test for testing illegal constructor arguments

--- test/dataStructures/LinkedHashTableTest.java
+++ test/dataStructures/LinkedHashTableTest.java
@@ -1,4 +1,5 @@
 package dataStructures;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
@@ -46,49 +47,59 @@ class LinkedHashTableTest {
 	}
 
 	/**
-	 * Test method for {@link dataStructures.LinkedHashTable#LinkedHashTable(java.lang.Class, int)}.
+	 * Test method for
+	 * {@link dataStructures.LinkedHashTable#LinkedHashTable(java.lang.Class, int)}.
 	 */
 	@Test
 	final void testLinkedHashTable() {
-		fail("Not yet implemented"); // TODO
+		try {
+			new LinkedHashTable<String>(0);
+			fail("Exception was expected for 0 input");
+		} catch (IllegalArgumentException e) {
+		}
+
+		try {
+			new LinkedHashTable<String>(-1);
+			fail("Exception was expected for negative input");
+		} catch (IllegalArgumentException e) {
+		}
 	}
-	
+
 	@Test
 	final void testInsert() {
 		// Create a linked list based hash table that stores strings
 		LinkedHashTable<String> htWords = new LinkedHashTable<String>(5000);
-		
+
 		// Insert the word 'Hello'
 		htWords.insert("Hello");
-		
+
 		// Check that it exists in the hash table
 		assertEquals(true, htWords.exists("Hello"));
 	}
-	
+
 	@Test
 	final void testDelete() {
 		LinkedHashTable<String> htWords = new LinkedHashTable<String>(5000);
-		
+
 		// Insert the word 'Hello'
 		htWords.insert("Hello");
-		
+
 		// Delete it from the hash table
 		htWords.delete("Hello");
-		
+
 		// Check that it no longer exists
 		assertEquals(false, htWords.exists("Hello"));
 	}
-	
+
 	@Test
 	final void testCount() {
 		LinkedHashTable<String> htWords = new LinkedHashTable<String>(5000);
-		
-		
+
 		// Insert 500 'Test' strings
-		for(int i=0; i<500; i++) {
+		for (int i = 0; i < 500; i++) {
 			htWords.insert("Test");
 		}
-		
+
 		// check that we have a count of 500 test objects
 		assertEquals(500, htWords.getCount("Test"));
 	}
