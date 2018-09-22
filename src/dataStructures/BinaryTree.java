412db29ad0f49703e90ec8e6a2783ad4eb5296b0
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-22 01:30:39 +0100

    updated; BinaryTree.java; added root node and constructor to save key into root

--- src/dataStructures/BinaryTree.java
+++ src/dataStructures/BinaryTree.java
@@ -1,5 +1,10 @@
 package dataStructures;
 
-public class BinaryTree {
-
+public class BinaryTree<T> {
+	// root node of the tree
+	Node<T> root;
+	
+	public BinaryTree(T key) {
+		root = new Node<T>(key);
+	}
 }
