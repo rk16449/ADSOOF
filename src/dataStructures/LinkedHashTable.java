95a44a755b7f2a5366d394488afddbef98c462f7
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-22 01:17:26 +0100

    updated; LinkedHashTable.java

--- src/dataStructures/LinkedHashTable.java
+++ src/dataStructures/LinkedHashTable.java
@@ -7,87 +7,95 @@ public class LinkedHashTable<T> extends SuperHash<T> implements IHashTable<T> {
 	// Generic Hash Table that supports separate chaining
 	// Uses a prime number as array size
 	// Underlining data structure is an array
-	
-	LinkedList<T>[] myArray;
-	
+
+	LinkedList<T>[] rgLL;
+
 	@SuppressWarnings("unchecked")
 	public LinkedHashTable(int size) {
-		
+
+		// Validate arguments
 		validateSize(size);
-		
+
 		// Make sure its prime sized
 		arraySize = getNextPrime(size);
 
-		// Try to set it normally?
-		myArray = new LinkedList[arraySize];
-		
-		System.out.println("Size of normal linked list generic: " + myArray.length);
-		
-		count = 0;
+		// Initialize range of Linked Lists
+		rgLL = new LinkedList[arraySize];
+
+		// Set the entries value to 0
+		entries = 0;
 	}
-	
+
 	private void validateSize(int size) {
-		if(size <= 0) throw new IllegalArgumentException("Invalid size entered!");
+		if (size <= 0)
+			throw new IllegalArgumentException("Invalid size entered!");
+	}
+
+	private void isHashTableFull() {
+		if (entries >= arraySize)
+			throw new IndexOutOfBoundsException("Hash Table is full!");
+	}
+
+	private boolean isDuplicate(T obj, int i) {
+		boolean found = false;
+
+		// check if its a duplicate by looping through the linked list
+		for (Cell<T> ptr = rgLL[i].getList(); ptr != null; ptr = ptr.getNext()) {
+			// we have a match
+			if (ptr.getFirst().equals(obj)) {
+				// increase its count
+				ptr.inc();
+				found = true;
+				break;
+			}
+		}
+
+		return found;
 	}
 
 	public void insert(T obj) {
-		// calculate hash indexes
-		// calculate the step required
+		// Check hash table isn't full
+		isHashTableFull();
 
-		if (count >= arraySize)
-			throw new IndexOutOfBoundsException("Hash Table is full!");
-
-		int hashIndex = hashIndexOne(obj);
-		
-		System.out.println("Hash index to check insert: " + hashIndex);
+		// Calculate hash index
+		int i = hashIndexOne(obj);
 
 		// if it isn't null then we have a linked list, just add it to the linked list
-		if(myArray[hashIndex] != null) {
-			
-			boolean found = false;
-			// check if its a duplicate by looping through the linked list
-			for(Cell<T> ptr = myArray[hashIndex].getList(); ptr != null; ptr = ptr.getNext()) {
-				
-				// we have a match, increase its count
-				if(ptr.getFirst().equals(obj)) {
-					ptr.inc();
-					found = true;
-					break;
-				}
+		if (rgLL[i] != null) {
+			// Check if its a duplicate (increases its count,
+			// if its not a duplicate, it'll attach as a new entry
+			if (!isDuplicate(obj, i)) {
+				rgLL[i] = rgLL[i].cons(obj);
 			}
-			
-			// We didn't find anything in the linked list, so just insert it into the linked list
-			if(!found) {
-				myArray[hashIndex] = myArray[hashIndex].cons(obj);
-			}
-		}else {
+		} else {
 			// add a new linked list in this element
-			myArray[hashIndex] = new LinkedList<T>(new Cell<T>(obj, null));
+			rgLL[i] = new LinkedList<T>(new Cell<T>(obj, null));
 		}
 	}
 
 	public void delete(T obj) {
-		
+
 		// Calculate hash value
-		int hashIndex = hashIndexOne(obj);
-		
-		System.out.println("Deleteing at index: " + hashIndex);
-		
+		int i = hashIndexOne(obj);
+
+		System.out.println("Deleteing at index: " + i);
+
 		// check there is something on the index
-		if(myArray[hashIndex] != null) {
-			
+		if (rgLL[i] != null) {
+
 			System.out.println("Linked List exists at index;");
-			
-			// check in the LinkedList if the obj exists there, if it does check its count value, dec if > 1
-			
-			for(Cell<T> ptr = myArray[hashIndex].getList(); ptr != null; ptr = ptr.getNext()) {
+
+			// check in the LinkedList if the obj exists there, if it does check its count
+			// value, dec if > 1
+
+			for (Cell<T> ptr = rgLL[i].getList(); ptr != null; ptr = ptr.getNext()) {
 				// is there a match?
-				if(ptr.getFirst().equals(obj)) {
+				if (ptr.getFirst().equals(obj)) {
 					// check the count, decrement it, else delete it
-					if(ptr.getCount() > 1) {
+					if (ptr.getCount() > 1) {
 						ptr.dec();
-					}else {
-						myArray[hashIndex].delete(obj);
+					} else {
+						rgLL[i].delete(obj);
 					}
 					break;
 				}
@@ -96,60 +104,63 @@ public class LinkedHashTable<T> extends SuperHash<T> implements IHashTable<T> {
 	}
 
 	public void update(T obj1, T obj2) {
-		// calculate the hash index
-		
-		// find obj1
 
-		// if obj1 is found then remove it entirely, and re-insert obj2 into the linked
-		// list
+		// calculate the hash index
+		int i = hashIndexOne(obj1);
+
+		if (rgLL[i] != null) {
+
+		}
+
+		// if obj1 is found then remove it entirely, and re-insert obj2
 	}
-	
+
 	// Returns the count of a certain object in the Hash Table / Linked List
 	public int getCount(T obj) {
-		
+
 		// calculate hash value
-		int hashIndex = hashIndexOne(obj);
-		
-		if(myArray[hashIndex] != null) {
-			
+		int i = hashIndexOne(obj);
+
+		if (rgLL[i] != null) {
+
 			// Loop through the linked list until obj is found
-			for(Cell<T> ptr = myArray[hashIndex].getList(); ptr != null; ptr = ptr.getNext()) {
-				
+			for (Cell<T> ptr = rgLL[i].getList(); ptr != null; ptr = ptr.getNext()) {
+
 				// Match found, return the count value!
-				if(ptr.getFirst().equals(obj)) {
+				if (ptr.getFirst().equals(obj)) {
 					return ptr.getCount();
 				}
 			}
 		}
-		
+
 		// Else no count found
 		return 0;
 	}
 
 	public void updateCount(T obj, int count) {
 		// calculate hash/index
-		int hashIndex = hashIndexOne(obj);
+		int i = hashIndexOne(obj);
 	}
-		// set the value of the count of the obj if found
+	// set the value of the count of the obj if found
 
 	@Override
 	public boolean exists(T obj) {
-		
+
 		// calculate hash/step
-		int hashIndex = hashIndexOne(obj);
-		
-		System.out.println("Hash index to check exists: " + hashIndex);
+		int i = hashIndexOne(obj);
+
+		System.out.println("Hash index to check exists: " + i);
 
 		// check if this index is not null
-		if(myArray[hashIndex] != null) {
-			
+		if (rgLL[i] != null) {
+
 			// if its not null check inside the LinkedList if the value exists
-			for(Cell<T> ptr = myArray[hashIndex].getList();  ptr != null; ptr = ptr.getNext()) {
-				
-				if(ptr.getFirst().equals(obj)) {
+			for (Cell<T> ptr = rgLL[i].getList(); ptr != null; ptr = ptr.getNext()) {
+
+				if (ptr.getFirst().equals(obj)) {
 					return true;
 				}
-				
+
 			}
 		}
 		return false;
