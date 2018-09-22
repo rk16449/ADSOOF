79b6dcce9e510c92b25bf66747ce43b505a3ee96
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-22 01:29:16 +0100

    updated; Node.java; attributes and constructor added

--- src/dataStructures/Node.java
+++ src/dataStructures/Node.java
@@ -1,5 +1,13 @@
 package dataStructures;
 
-public class Node {
-
+public class Node<T> {
+	
+	T key;
+	Node<T> left, right;
+	
+	public Node(T key) {
+		this.key = key;
+		// set left and right nodes to null
+		left = right = null;
+	}
 }
