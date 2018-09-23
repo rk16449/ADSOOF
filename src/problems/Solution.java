688c3be3fd56d8aaa4a0f7b6489cf9284922e636
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-23 15:12:36 +0100

    updated; Solution.java; solved the problem

--- src/problems/Solution.java
+++ src/problems/Solution.java
@@ -10,46 +10,94 @@ import java.util.regex.*;
 
 public class Solution {
 
-    // Complete the hourglassSum function below.
-    static int hourglassSum(int[][] arr) {
-    	
-    	// Loop the hour glasses which is it needs to have a 3 element width on rows, and 3 element height
-    	for(int i=0; i<arr.length; i++) {
-    		
- 
-    		for(int j=0; j<arr.length; j++) {
-    			
-    		}
-
-    	}
-    	
-    	return 0;
-    }
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int[][] arr = new int[6][6];
-
-        for (int i = 0; i < 6; i++) {
-            String[] arrRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < 6; j++) {
-                int arrItem = Integer.parseInt(arrRowItems[j]);
-                arr[i][j] = arrItem;
+	// Complete the hourglassSum function below.
+	static int hourglassSum(int[][] arr) {
+		
+		int highestSum = 0;
+		
+		// Subset placeholder values
+		int[][] subset = new int[][]{
+				{0,  0,  0},
+				{0,  0,   0},
+				{0,  0,  0}
+		};
+		
+		// Loop the hour glasses; requires 3x width, 3x height,
+		for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+            	
+            	
+            	// Check that we have enough columns and rows from here
+            	if( ((row + 2) < arr.length) && ((col + 2) < arr.length)){
+            		
+            		System.out.print(arr[row][col] + "\t");
+            		
+            		// Manually looks like this;
+            		
+            		// Manual subset:
+            		subset[0][0] = arr[row][col];
+            		subset[0][1] = arr[row][col+1];
+            		subset[0][2] = arr[row][col+2];
+            		subset[1][1] = arr[row+1][col+1];
+            		subset[2][0] = arr[row+2][col];
+            		subset[2][1] = arr[row+2][col+1];
+            		subset[2][2] = arr[row+2][col+2];
+            		
+            		// Pass this subset into a method
+            		
+            		if(calcSum(subset) > highestSum) {
+            			highestSum = calcSum(subset);
+            		}
+            		
+            		
+            	}
             }
+            System.out.println();
         }
+		
+		System.out.println("Highest sum is: " + highestSum);
+		
+		return highestSum;
+	}
+	
+	static int calcSum(int[][] arr) {
+		
+		int sum = 0;
+		for(int row = 0; row < arr.length; row ++) {
+			for(int col = 0; col < arr[row].length; col++) {
+				sum += arr[row][col];
+			}
+		}
+		
+		return sum;
+	}
 
-        int result = hourglassSum(arr);
+	private static final Scanner scanner = new Scanner(System.in);
 
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
+	public static void main(String[] args) throws IOException {
+		//BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        bufferedWriter.close();
+		int[][] arr = new int[6][6];
+		
+	
+		
+		for (int i = 0; i < 6; i++) {
+			String[] arrRowItems = scanner.nextLine().split(" ");
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        scanner.close();
-    }
+			for (int j = 0; j < 6; j++) {
+				int arrItem = Integer.parseInt(arrRowItems[j]);
+				arr[i][j] = arrItem;
+			}
+		}
+		
+		int result = hourglassSum(arr);
+
+		//bufferedWriter.write(String.valueOf(result));
+		//bufferedWriter.newLine();
+
+		//bufferedWriter.close();
+
+		scanner.close();
+	}
 }
