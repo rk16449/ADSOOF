6f0b1b4ec5c717b2c7430506688132d27fc5dc6b
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-24 01:21:27 +0100

    solved; CompareTwoLinkedLists.java

--- src/problems/CompareTwoLinkedLists.java
+++ src/problems/CompareTwoLinkedLists.java
@@ -63,7 +63,15 @@ public class CompareTwoLinkedLists {
      *
      */
     static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
-    	return true;
+    	
+        // if we have nodes, loop through them
+    	while((head1 != null && head2 != null) && (head1.data == head2.data)) {
+    		head1 = head1.next;
+    		head2 = head2.next;
+    	}
+
+    	// compare heads
+    	return head1 == head2;
     }
     private static final Scanner scanner = new Scanner(System.in);
 
