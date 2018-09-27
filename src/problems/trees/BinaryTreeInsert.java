179c20dc89275df935bb9b938331d618d0ad13ed
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-27 02:02:25 +0100

    added; BinaryTreeInsert.java

---
+++ src/problems/trees/BinaryTreeInsert.java
@@ -0,0 +1,38 @@
+package problems.trees;
+
+import java.util.*;
+import java.io.*;
+
+class BinaryTreeInsert {
+
+	public static void preOrder(Node root) {
+
+		if (root == null)
+			return;
+
+		System.out.print(root.data + " ");
+		preOrder(root.left);
+		preOrder(root.right);
+
+	}
+	
+	/*
+	 * Node is defined as : class Node int data; Node left; Node right;
+	 * 
+	 */
+	public static Node insert(Node root, int data) {
+
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		preOrder(root);
+	}
+}
