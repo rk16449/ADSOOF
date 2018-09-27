6de6776b9a2aceab72e28354c690962ca48ea89f
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-27 01:31:01 +0100

    updated; PostOrder.java

--- src/problems/trees/PostOrder.java
+++ src/problems/trees/PostOrder.java
@@ -1,5 +1,68 @@
 package problems.trees;
 
-public class PostOrder {
+import java.util.*;
+import java.io.*;
 
+class Node {
+	Node left;
+	Node right;
+	int data;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
+
+class PostOrder {
+	/*
+	 * you only have to complete the function given below. Node is defined as
+	 * 
+	 * class Node { int data; Node left; Node right; }
+	 * 
+	 */
+
+	public static void postOrder(Node root) {
+
+		if (root == null)
+			return;
+
+		// traverse left subtree
+		postOrder(root.left);
+
+		// traverse right side
+		postOrder(root.right);
+
+		// print node
+		System.out.print(root.data + " ");
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		postOrder(root);
+	}
 }
