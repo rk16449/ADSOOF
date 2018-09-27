abe61fad0bdc2ea7cf4dab71fde4df004c46344d
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-27 01:31:21 +0100

    updated; PreOrder.java

--- src/problems/trees/PreOrder.java
+++ src/problems/trees/PreOrder.java
@@ -3,64 +3,49 @@ package problems.trees;
 import java.util.*;
 import java.io.*;
 
-class Node {
-    Node left;
-    Node right;
-    int data;
-    
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
+class PreOrder {
+	/*
+	 * you only have to complete the function given below. Node is defined as
+	 * 
+	 * class Node { int data; Node left; Node right; }
+	 * 
+	 */
 
-class Solution {
-/* you only have to complete the function given below.  
-Node is defined as  
+	public static void preOrder(Node root) {
+		// print out the binary tree down the left, then back up, then go to the right,
+		// if null go back up
+		if (root == null)
+			return;
+		System.out.println(root.data);
+		preOrder(root.left);
+		preOrder(root.right);
+	}
 
-class Node {
-    int data;
-    Node left;
-    Node right;
-}
-
-*/
-
-    public static void preOrder(Node root) {
-
-    	// print out the binary tree down the left, then back up, then go to the right, if null go back up
-    	
-    	if(root == null) return;
-    	System.out.println(root.data);
-    	preOrder(root.left);
-    	preOrder(root.right);
-    }
 	public static Node insert(Node root, int data) {
-        if(root == null) {
-            return new Node(data);
-        } else {
-            Node cur;
-            if(data <= root.data) {
-                cur = insert(root.left, data);
-                root.left = cur;
-            } else {
-                cur = insert(root.right, data);
-                root.right = cur;
-            }
-            return root;
-        }
-    }
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int t = scan.nextInt();
-        Node root = null;
-        while(t-- > 0) {
-            int data = scan.nextInt();
-            root = insert(root, data);
-        }
-        scan.close();
-        preOrder(root);
-    }	
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		preOrder(root);
+	}
 }
