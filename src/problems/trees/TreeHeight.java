946eb126fad545b4cb08aef6544ceb83e56939d0
Raj <32778886+rk16449@users.noreply.github.com>
2018-09-27 01:59:23 +0100

    added; TreeHeight.java

---
+++ src/problems/trees/TreeHeight.java
@@ -0,0 +1,43 @@
+package problems.trees;
+
+import java.util.*;
+import java.io.*;
+
+class TreeHeight {
+	/*
+	 * class Node int data; Node left; Node right;
+	 */
+	public static int height(Node root) {
+		// Write your code here.
+		return 0;
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		int height = height(root);
+		System.out.println(height);
+	}
+}
